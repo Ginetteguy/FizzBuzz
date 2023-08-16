17d0ca9a64fabcc4b711bc65d31290e8c2c01fb4
Axel GODIN <AxelGodin@groupeisagri.com>
2023-08-16 10:48:58 +0200

    New test for CodeRabbit

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -7,14 +7,17 @@ namespace FizzBuzz
     /// </summary>
     public class Program
     {
+        private const string _fizz = "Fizz";
+        private const string _buzz = "Buzz";
+        private const string _fizzBuzz = "FizzBuzz";
         public string FizzBuzz(int input)
         {
             if (input % 15 == 0)
-                return "FizzBuzz";
+                return _fizzBuzz;
             if (input % 3 == 0)
-                return "Fizz";
+                return _fizz;
             if (input % 5 == 0)
-                return "Buzz";
+                return _buzz;
             return input.ToString();
         }
     }
